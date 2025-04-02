eba3442c33427a17e8d3a731cd8802eba967ff89
Priya K R <priyarag04@gmail.com>
2025-04-02 15:06:58 +0530

    oh

--- cars_api_sample.py
+++ cars_api_sample.py
@@ -3,6 +3,6 @@ import requests
 base_url = "https://cars-app.qxf2.com/cars"
 username = "qxf2"
 password = "qxf2"
-response = requests.get("https://cars-app.qxf2.com/cars",auth=(username,password))
+response = requests.get(url=base_url + 'cars', auth=(username,password))
 
 print(response.status_code)
